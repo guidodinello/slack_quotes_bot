bb3376de8d32a401013deb1799b5fad8335e9f1b
Guido Dinello <guido.dinello@gmail.com>
2023-10-29 00:59:04 -0300

    runner script

--- runner.sh
+++ runner.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
+VENV="SLACK_ENV"
+PY_VERSION="3.10"
+SCRIPT="bot_slack.py"
 
 git pull
-. SLACK_ENV/bin/activate
-python3.10 bot_slack.py
+if [ ! -d "${VENV}" ]; then
+    echo "Creating virtual environment..."
+    python${PY_VERSION} -m venv "${VENV}"
+    # shellcheck source=./SLACK_ENV/bin/activate
+    . "./${VENV}/bin/activate"
+    pip3 install -r requirements.txt
+else
+    # shellcheck source=./SLACK_ENV/bin/activate
+    . "./${VENV}/bin/activate"
+fi
+python${PY_VERSION} "${SCRIPT}"
--- setuper.sh
+++
@@ -1,8 +0,0 @@
-VENV=SLACK_ENV
-PY_VERSION="3.10"
-
-python${PY_VERSION} -m venv "${VENV}"
-# shellcheck source=./SLACK_ENV/bin/activate
-. "./${VENV}/bin/activate"
-pip3 install -r requirements.txt
-# python${PY_VERSION} bot_slack.py
