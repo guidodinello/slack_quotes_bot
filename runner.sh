6538d0ba38c6946d181d3c134d5c6ccf6b013a38
Guido Dinello <guido.dinello@gmail.com>
2023-11-08 19:59:00 -0300

    improving runner script error messages

--- runner.sh
+++ runner.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 VENV="SLACK_ENV"
-PY_VERSION="3.10"
+PY_VERSION="3.12"
 SCRIPT="bot_slack.py"
 
 git pull
 if [ ! -d "${VENV}" ]; then
     echo "Creating virtual environment..."
-    python${PY_VERSION} -m venv "${VENV}"
+    python${PY_VERSION} -m venv "${VENV}" || (echo "Failed to create virtual environment" && exit 1)
     # shellcheck source=./SLACK_ENV/bin/activate
-    . "./${VENV}/bin/activate"
+    . "./${VENV}/bin/activate" || (echo "Failed to activate virtual environment" && exit 1)
     pip3 install -r requirements.txt
 else
     # shellcheck source=./SLACK_ENV/bin/activate
