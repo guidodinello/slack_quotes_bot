db373e6313239169caaf38089c919d7d06c9c582
Guido Dinello <guido.dinello@gmail.com>
2023-11-10 20:03:06 -0300

    support for deleting a msg and getting all channel messages

---
+++ utils.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import argparse
+
+import json
+
+load_dotenv()
+
+slack_bot_token = os.environ.get("SLACK_BOT_TOKEN")
+channel_id = os.environ.get("TO_CHANNEL_ID")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Delete a message from a channel")
+    parser.add_argument(
+        "--delete", nargs=1, type=str, help="The ts of the message you want to delete"
+    )
+    parser.add_argument(
+        "--get_all", action="store_true", help="Get all messages from a channel"
+    )
+    args = parser.parse_args()
+
+    if not slack_bot_token:
+        slack_bot_token = input("SLACK_BOT_TOKEN:(example xoxb-...-...):")
+    if not channel_id:
+        channel_id = input("CHANNEL_ID (example C01B4PVGLVB):")
+
+    client = WebClient(token=slack_bot_token)
+
+    try:
+        if args.delete:
+            message_id = args.delete[0]
+            # NOTE: client_msg_id used by bot_slack is not the same as ts
+            result = client.chat_delete(channel=channel_id, ts=args.message_id)
+
+        if args.get_all:
+            response = client.conversations_history(channel=channel_id)
+            json.dump(response.data, open("messages.json", "w"), indent=4)
+
+    except SlackApiError as e:
+        print(f"Error: {e}")
